958348938dc6966f28c444805f84271e841bac14
EvelyneBerrios <evelyne.berrios@hotmail.com>
2016-02-18 14:23:36 -0600

    Proyecto 4

---
+++ Programa 4/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+/*
+Ingresar dos numeros y determinar cual es el numero
+mayor
+*/
+
+int main()
+{ int num1,num2,mayor;
+printf("Ingresar numero..:1");
+scanf("%d", &num1);
+printf("Ingresar numero..:2");
+scanf("%d", &num2);
+
+if (num1>num2)
+{
+    mayor = num1;
+}
+else
+{
+    mayor = num2;
+}
+printf("Numero mayor es %d \n",mayor);
+
+    return 0;
+}
